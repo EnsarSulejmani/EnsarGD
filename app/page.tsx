68adc1b993e59ec514bcad1db5f5083b3bd82aa9
Ensar Sulejmani <149506033+EnsarSulejmani@users.noreply.github.com>
2024-09-26 23:45:21 +0200

    Created NextJS Project

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <div>
+        This is the website for all threeJS projects for the course "Computer
+        Graphics"
+      </div>
+    </div>
+  );
+}
